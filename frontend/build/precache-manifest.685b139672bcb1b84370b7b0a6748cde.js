e413ba595cb53740688d0bf4e1f374e4982ce04c
Dip Gurung <deepgrg03@gmail.com>
2020-06-25 14:07:56 +0545

    The build file

---
+++ frontend/build/precache-manifest.685b139672bcb1b84370b7b0a6748cde.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91957d55d3de0a66124d4064cc584a57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "074f35ae9ecf7ea2817a",
+    "url": "/static/css/main.c608f838.chunk.css"
+  },
+  {
+    "revision": "eed458071b0ccc611bae",
+    "url": "/static/js/2.654c3174.chunk.js"
+  },
+  {
+    "revision": "79c21b60b7bd6d507c431e5cb9a5563e",
+    "url": "/static/js/2.654c3174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074f35ae9ecf7ea2817a",
+    "url": "/static/js/main.52c4e674.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "4447cefb0314d0ca4ac15d01cd2c6987",
+    "url": "/static/media/Me.4447cefb.svg"
+  },
+  {
+    "revision": "a605f620e7a89c357afd09188b370f21",
+    "url": "/static/media/ProfileMe.a605f620.jpg"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "d0b9bb369a2ee0b372063e88b5e60b83",
+    "url": "/static/media/avatar.d0b9bb36.svg"
+  },
+  {
+    "revision": "f14635195c4f705261b7e7cd2a958a15",
+    "url": "/static/media/pageNotFound.f1463519.svg"
+  }
+]);
